4ea7da9a90fe3050d202950b341c80b990323b75
Ayomide Victor <midevictor90@gmail.com>
2022-07-11 15:36:28 +0100

    Time: 69 ms (95.51%), Space: 43 MB (36.83%) - LeetHub

---
+++ 1-two-sum/1-two-sum.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+// let result = [];
+var twoSum = function(nums, target) {
+//     for(let i = 0; i < nums.length; i++){
+//         for(let j = i + 1; j < nums.length; j++){
+//             if(nums[i] + nums[j] === target){
+//                 result.push(i);
+//                 result.push(j);
+//             }
+//         }
+         
+//     }
+//      return result;
+    let map = new Map();
+    for (let i = 0; i < nums.length; i++){
+        let num1 = nums[i];
+        let num2 = target - num1;
+        if(map.has(num2))
+            {
+                return[i, map.get(num2)];
+            }
+        map.set(num1, i)
+    }
+         
+    
+};
