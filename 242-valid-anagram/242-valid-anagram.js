85b3199f13303aa2b069ca527b37fe524b3fff89
Ayomide Victor <midevictor90@gmail.com>
2022-08-15 12:48:58 +0100

    Time: 160 ms (19.73%), Space: 43.1 MB (79.24%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -4,14 +4,41 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    let newS = s.split("").sort().join();
-    let newT = t.split("").sort().join();
+    // let newS = s.split("").sort().join();
+    // let newT = t.split("").sort().join(); 
+    // if( newS == newT){
+    //     return true;
+    // }
+    // else{
+    //     return false;
+    // }
+    //Was not optimized
     
-    if( newS == newT){
-        return true;
-    }
-    else{
+    
+    //an if statement to check if length of the two strings are equal
+    if (s.length !== t.length) {
         return false;
     }
+    // Created an empty object
+    let sMap = {};
+    let tMap = {};
+    for (let i = 0; i < s.length; i++) {
+        if (sMap.hasOwnProperty(s[i])) {
+            sMap[s[i]]++;
+        } else {
+            sMap[s[i]] = 1;
+        }
+        if (tMap.hasOwnProperty(t[i])) {
+            tMap[t[i]]++;
+        } else {
+            tMap[t[i]] = 1;
+        }
+    }
+    for (let ch in sMap) {
+        if (sMap[ch] !== tMap[ch]) {
+            return false;
+        }
+    }
+    return true;
     
 };
