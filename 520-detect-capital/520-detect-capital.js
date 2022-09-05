f89bfd2d96165f9e7ba21f09e25ef80372c06e0a
Ayomide Victor <midevictor90@gmail.com>
2022-09-05 10:42:29 +0100

    Time: 64 ms (95.29%), Space: 42.7 MB (19.96%) - LeetHub

--- 520-detect-capital/520-detect-capital.js
+++ 520-detect-capital/520-detect-capital.js
@@ -3,23 +3,14 @@
  * @return {boolean}
  */
 var detectCapitalUse = function(word) {
-//     let capitalCount = 0
-//     for( let i = 0; i < word.length; i++){
-//         if(word[i].toUpperCase() === word[i]){
-//             capitalCount++;
-//         }
-    
-//     if(capitalCount === word.length || capitalCount === 0 || (capitalCount === 1 && word[0].toUpperCase() === word[0]) ){  return true;}
-     
-//     }  
-    
-        let capCount = 0;
+    let capitalCount = 0;
     for (let i = 0; i < word.length; i++) {
         if (word[i].toUpperCase() === word[i]) {
-            capCount++;
+            capitalCount++;
         }
     }
-    return capCount === word.length || capCount === 0 || (capCount === 1 &&
+    return capitalCount === word.length || capitalCount === 0 || (capitalCount === 1 &&
         word[0].toUpperCase() === word[0]);
     
+
 };
