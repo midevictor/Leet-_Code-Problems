09476a6662eacebc59ad6a89ff8ef1e5442e80c0
Ayomide Victor <midevictor90@gmail.com>
2022-12-29 22:46:31 +0100

    Time: 83 ms (76.67%), Space: 43 MB (91.83%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function(n) {
+ if(n<1){
+     return false;
+ }
+    while(n>1){
+        if(n%2 != 0){
+            return false
+        }
+        else{
+            n = n/2;
+        }
+    }
+    return true;
+    
+    
+};
