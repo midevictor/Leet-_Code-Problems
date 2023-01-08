579877211abba7a1cded1c813725d9ec6c51761f
Ayomide Victor <midevictor90@gmail.com>
2023-01-08 20:44:30 +0100

    Time: 62 ms (90.77%), Space: 41.3 MB (98.25%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} heights
+ * @return {number}
+ */
+var heightChecker = function(heights) {
+  let copy = [...heights].sort((a, b) => a - b);
+  let count = 0;
+  for (let i = 0; i < heights.length; i++) {
+    if (heights[i] !== copy[i]) {
+      count++;
+    }
+  }
+  return count;
+    
+};
