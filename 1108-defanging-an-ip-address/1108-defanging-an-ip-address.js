32a13856223dfcb8e6c355e66530efe1ccb712ab
Ayomide Victor <midevictor90@gmail.com>
2023-01-13 23:52:58 +0100

    Time: 70 ms (61.74%), Space: 42 MB (28.82%) - LeetHub

---
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} address
+ * @return {string}
+ */
+var defangIPaddr = function(address) {
+    
+    // let res = address.replace(/\./g, '[.]')
+    // return res;
+ 
+    let result = address.split('')
+    for(let i=0;i<result.length;i++){
+        if(result[i] == '.'){
+         result[i] = '[.]'   
+        }
+    }
+   
+    return result.join("")
+};
