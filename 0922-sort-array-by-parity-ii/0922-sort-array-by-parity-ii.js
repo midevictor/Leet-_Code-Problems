1433dfd02131d2a1878bde72a301054df33fdd2a
Ayomide Victor <midevictor90@gmail.com>
2023-01-15 11:39:16 +0100

    Time: 281 ms (7.86%), Space: 53.2 MB (5.36%) - LeetHub

---
+++ 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArrayByParityII = function(nums) {
+  let evenNums = nums.filter(n => n%2===0);
+  let oddNums = nums.filter(n => n%2!==0);
+    let newArray = []
+    
+    for(let i = 0; i < evenNums.length; i++){
+        newArray.push( evenNums[i], oddNums[i]);
+        console.log(newArray[2]);
+        
+    }
+    return newArray
+    
+};
