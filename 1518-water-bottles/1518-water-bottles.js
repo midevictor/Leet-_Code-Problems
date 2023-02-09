d3a4383ea8b24c20bee5d6991e6cd31fc1e438bf
Ayomide Victor <midevictor90@gmail.com>
2023-02-09 08:54:21 +0100

    Time: 74 ms (39.34%), Space: 41.7 MB (68.85%) - LeetHub

--- 1518-water-bottles/1518-water-bottles.js
+++ 1518-water-bottles/1518-water-bottles.js
@@ -7,11 +7,11 @@ var numWaterBottles = function(numBottles, numExchange) {
     let total = 0
     let numEmpty = 0 
     while (numBottles || numEmpty >= numExchange) {
-        total+= numBottles
-        numEmpty+=numBottles 
-        numBottles = Math.floor(numEmpty / numExchange)
-        numEmpty = numEmpty % numExchange 
+        total+= numBottles;
+        numEmpty+=numBottles; 
+        numBottles = Math.floor(numEmpty / numExchange);
+        numEmpty = numEmpty % numExchange;
     }
-    return total
+    return total;
     
 };
