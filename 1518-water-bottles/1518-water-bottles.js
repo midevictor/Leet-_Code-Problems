a17532213e6aa71e571aaebeeca11051f7f474f4
Ayomide Victor <midevictor90@gmail.com>
2023-02-09 23:11:48 +0100

    Time: 58 ms (85.25%), Space: 41.8 MB (57.38%) - LeetHub

--- 1518-water-bottles/1518-water-bottles.js
+++ 1518-water-bottles/1518-water-bottles.js
@@ -4,14 +4,30 @@
  * @return {number}
  */
 var numWaterBottles = function(numBottles, numExchange) {
-    let total = 0
-    let numEmpty = 0 
-    while (numBottles || numEmpty >= numExchange) {
-        total+= numBottles;
-        numEmpty+=numBottles; 
-        numBottles = Math.floor(numEmpty / numExchange);
-        numEmpty = numEmpty % numExchange;
+    // let total = 0;
+    // let numEmpty = 0 ;
+    // while (numBottles || numEmpty >= numExchange) {
+    //     total+= numBottles;
+    //     numEmpty+=numBottles; 
+    //     numBottles = Math.floor(numEmpty / numExchange);
+    //     numEmpty = numEmpty % numExchange;
+    // }
+    // return total;   
+    let maxDrink = numBottles;
+    let remainder = 0;
+    while(numBottles >= numExchange){
+        remainder = numBottles % numExchange;
+        numBottles  = Math.floor( numBottles / numExchange);
+        maxDrink += numBottles;
+        numBottles += remainder;
+        
     }
-    return total;
-    
+    return maxDrink;
 };
+// create a variable for max drink
+//create a varible for remainder
+// a while loop( numBotttles >= numExchange)
+// remainder = numBottle  mod numExchange
+// numBottles / numExchange
+// add remainder to numBottles
+// return max Drink
