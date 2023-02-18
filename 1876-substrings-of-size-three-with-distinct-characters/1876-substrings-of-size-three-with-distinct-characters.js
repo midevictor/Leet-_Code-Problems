0e8284dce769b8419eff54d532d7a498f5ae0af7
Ayomide Victor <midevictor90@gmail.com>
2023-02-18 23:38:22 +0100

    Time: 70 ms (69.88%), Space: 44.5 MB (34.25%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.js
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.js
@@ -3,33 +3,33 @@
  * @return {number}
  */
 var countGoodSubstrings = function(s) {
-//     let count = 0;
-//     let result = 0;
-//     let string = "";
+    let count = 0;
+    let result = 0;
+    let string = "";
     
-//     for ( let i = 0; i < s.length; i++){
-//         count++;
-//         string = string + s[i];
-//         if(count === 3){
-//             if([...new Set(string.split(""))].length === 3){
-//                 result++;
-//                 count--;
-//                 string = string.slice(1);
-//             }
-//         }
-//     }
-//     return result;
-    let count = 0, result= 0, str = '';
-    for(let i =0; i< s.length; i++) {
+    for ( let i = 0; i < s.length; i++){
         count++;
-        str = str+ s[i];
-        if(count === 3) {
-            if([...new Set(str.split(''))].length === 3) result++;
-            count--;
-            str = str.slice(1);
+        string = string + s[i];
+        if(count === 3){
+            if([...new Set(string.split(""))].length === 3)
+                result++;
+                count--;
+                string = string.slice(1);
+            
         }
     }
     return result;
+    // let count = 0, result= 0, str = '';
+    // for(let i = 0; i< s.length; i++) {
+    //     count++;
+    //     str = str+ s[i];
+    //     if(count === 3) {
+    //         if([...new Set(str.split(''))].length === 3) result++;
+    //         count--;
+    //         str = str.slice(1);
+    //     }
+    // }
+    // return result;
     
 };
     
