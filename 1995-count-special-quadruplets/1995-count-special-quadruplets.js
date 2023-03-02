16ba9ebe7577fc81492c06b4a9a57dd889609248
Ayomide Victor <midevictor90@gmail.com>
2023-03-02 23:17:46 +0100

    Time: 79 ms (76.74%), Space: 42.6 MB (34.88%) - LeetHub

---
+++ 1995-count-special-quadruplets/1995-count-special-quadruplets.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var countQuadruplets = function(nums) {
+    let count = 0;
+    for ( let i = 0; i < nums.length; i++){
+        for(let j = i + 1; j < nums.length; j++){
+            for(let k = j+1; k < nums.length - 1; k++){
+                for (let l = k + 1; l < nums.length; l++ ){
+                    if (nums[i] + nums[j] + nums[k] === nums[l]){
+                        count++;
+                    }
+                }
+            }
+            
+        }
+        
+    }
+    return count;
+    
+  
+};
