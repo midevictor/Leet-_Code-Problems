48bd5f5952d3181679b09e997051bb363fca2912
Ayomide Victor <midevictor90@gmail.com>
2023-05-17 12:10:32 +0100

    Time: 193 ms (53.43%), Space: 58.3 MB (43.55%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -3,26 +3,33 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
+    ///cretaes an empty array for the sum
     let newArray = [];
+    //initilaize position of left
     let left = 0;
+    //initialize the position  of right
     let right = nums.length-1;
+    // if the length of nums is less than 3
     if(nums.length < 3){
         return newArray;
     }
+    //sort nums
     nums.sort((a,b)=>
               {return a-b});
     
     for(let [index, num] of nums.entries()){
+        //checks if the first num > 0 cos the other nums will be gretaer than it and that wont lead to a sum of 0
         if(num > 0){
             return newArray;
         }
+        //checks if the current num is not equal to the previous
         if(num === nums[index-1]){
             continue;
         }
         left = index+1;
         right = nums.length-1;
         let temp = 0;
-        
+        //while left position is less than right
         while(left < right){
             temp = num + nums[left]+ nums[right];
             
@@ -37,9 +44,11 @@ var threeSum = function(nums) {
                     right--
                 }
             }
+            //sum too big
             else if (temp > 0){
                 right--
             }
+            //sum too small
             else if (temp < 0){
                 left++
             }
