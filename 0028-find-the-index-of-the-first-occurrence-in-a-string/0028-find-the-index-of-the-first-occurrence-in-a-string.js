b7112e789e6f5a65aaa0c7c0051dc32db7459965
Ayomide Victor <midevictor90@gmail.com>
2023-05-23 22:25:47 +0100

    Time: 58 ms (61.72%), Space: 41.5 MB (88.51%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    let result = haystack.indexOf(needle);
+    return result
+    
+};
