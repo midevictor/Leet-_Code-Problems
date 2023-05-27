46945e31977a6fad491e9c695c6ec8e1c30b44a7
Ayomide Victor <midevictor90@gmail.com>
2023-05-27 17:43:01 +0100

    Time: 63 ms (61.34%), Space: 44.8 MB (15.38%) - LeetHub

---
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[]} details
+ * @return {number}
+ */
+var countSeniors = function(details) {
+    let count = 0;
+    
+    for ( let detail of details){
+        let age = detail.slice(-4,-2);
+        if(age>60){
+            count++;
+        }
+        
+    }
+    return count;
+    
+};
+
