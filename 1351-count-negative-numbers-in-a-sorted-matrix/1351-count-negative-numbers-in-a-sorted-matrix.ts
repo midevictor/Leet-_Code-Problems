43890b76fd22b2743669d54875eb60c936773372
Ayomide Victor <midevictor90@gmail.com>
2023-06-08 23:53:37 +0100

    Time: 66 ms (58.06%), Space: 45.9 MB (15.05%) - LeetHub

---
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.ts
@@ -0,0 +1,21 @@
+function countNegatives(grid: number[][]): number {
+    let count = 0;
+    for(let i=0; i < grid.length; i++){
+        for(let j = 0; j < grid[0].length; j++){
+            if(grid[i][j] < 0){
+                count++;
+            }
+        }    
+        
+    }
+    return count;
+
+};
+
+// let count = 0;
+//     for(let i=0; i<grid.length; i++){
+//         for(let j=0; j<grid[0].length; j++){
+//             if(grid[i][j] < 0)  count++;
+//         }
+//     }
+//     return count;
