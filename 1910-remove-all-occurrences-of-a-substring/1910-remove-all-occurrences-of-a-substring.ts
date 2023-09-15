eafb394e52321137f868472350a0cabd2b4dc67e
Ayomide Victor <midevictor90@gmail.com>
2023-09-15 23:33:07 +0100

    Time: 59 ms (62.50%), Space: 44.7 MB (25.00%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.ts
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.ts
@@ -1,7 +1,12 @@
 function removeOccurrences(s: string, part: string): string {
-    while(s.includes(part)){
-        s = s.replace(part, "");
-    }
-    return s;
+    // while(s.includes(part)){
+    //     s = s.replace(part, "");
+    // }
+    // return s;
+  let res = s;
+  for(let i = 0; i < s.length; i++) {
+    res = res.replace(part, "");
+  }
+  return res;
 
 };
