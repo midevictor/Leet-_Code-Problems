2505fd7f802abd54ab241630fc08ae93b4d9e2dd
Ayomide Victor <midevictor90@gmail.com>
2023-09-18 22:36:20 +0100

    Time: 81 ms (95.63%), Space: 56.2 MB (35.86%) - LeetHub

---
+++ 0575-distribute-candies/0575-distribute-candies.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number[]} candyType
+ * @return {number}
+ */
+var distributeCandies = function(candyType) {
+     return Math.min(new Set(candyType).size, candyType.length / 2);
+    
+};
