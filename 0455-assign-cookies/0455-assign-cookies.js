a7fb265bfb56831e364ec7c89e40c40e805dd5bf
Ayomide Victor <midevictor90@gmail.com>
2024-01-01 22:40:48 +0100

    Time: 121 ms (10.28%), Space: 44.1 MB (91.36%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} g
+ * @param {number[]} s
+ * @return {number}
+ */
+var findContentChildren = function(g, s) {
+    g.sort((a,b) => a-b);
+    s.sort((a,b) => a-b);
+    
+    let count = 0;
+    let index = 0;
+    
+    while (index < s.length && count < g.length ){
+        if(s[index] >= g[count]){
+            count++;
+        }
+        index++
+    }
+    
+    return count;
+    
+};
