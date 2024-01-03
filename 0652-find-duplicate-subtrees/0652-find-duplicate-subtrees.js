e741b352ba109ef1cc0930ce509ae4ce8e761581
Ayomide Victor <midevictor90@gmail.com>
2024-01-03 23:30:34 +0100

    Time: 81 ms (94.29%), Space: 46.9 MB (90.00%) - LeetHub

--- 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.js
+++ 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.js
@@ -11,16 +11,21 @@
  * @return {TreeNode[]}
  */
 var findDuplicateSubtrees = function(root) {
+    //create a map to store subtres and their occurence
     let map = new Map();
+    //an array to store the given results
     let result = [];
     
+    //performs the postorder traversal
     let postOrder = function(curr){
+        //if no current value, returns an empty string
         if(!curr){
             return ""
         }
         let leftSubTree = postOrder(curr.left);
         let rightSubTree = postOrder(curr.right);
         let structure = `${curr.val}_${leftSubTree}_${rightSubTree}`
+        // console.log(structure);
         map.set(structure, (map.get(structure) || 0) + 1);
         if(map.get(structure) == 2){
             result.push(curr)
