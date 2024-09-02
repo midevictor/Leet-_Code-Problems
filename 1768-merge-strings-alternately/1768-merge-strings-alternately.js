b255e29d05b33a018b75a69e4f2519f76fc0b1f0
Ayomide Victor <midevictor90@gmail.com>
2024-09-02 16:07:11 -0700

    Time: 48 ms (84.37%), Space: 50.3 MB (20.64%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.js
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.js
@@ -8,7 +8,9 @@ var mergeAlternately = function(word1, word2) {
     //loop through the max length of the two words to get the maximum length;
     let maxLength = Math.max(word1.length, word2.length);
     for (let i = 0; i < maxLength; i++){
+        // take a character in the word1 and push into the result array
         if(word1[i])result.push(word1[i]);
+        // take a character in word2 and push into the result array
         if(word2[i])result.push(word2[i]);
     }
     return result.join("")   
