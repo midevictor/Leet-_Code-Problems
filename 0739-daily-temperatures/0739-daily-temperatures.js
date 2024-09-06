bc8a6ed2b854e2a7fd7953c67f7615700bbd70dc
Ayomide Victor <midevictor90@gmail.com>
2024-09-06 05:39:23 -0700

    Time: 225 ms (35.17%), Space: 75.4 MB (33.70%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} temperatures
+ * @return {number[]}
+ */
+var dailyTemperatures = function(temp) {
+    let stack = [];
+    let result = new Array(temp.length).fill(0);
+    
+    for(let i = temp.length-1; i >= 0; i--){
+        while(stack.length && temp[stack[stack.length-1]] <= temp[i]){
+            stack.pop();
+        }
+        if(stack.length){
+            result[i] = stack[stack.length-1] - i;
+        }
+        
+        stack.push(i);
+        
+    }
+    
+    return result
+    
+};
