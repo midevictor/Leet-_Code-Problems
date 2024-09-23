8192c08032aa6f7146aab7ced365bc7d49e3dbd7
Ayomide Victor <midevictor90@gmail.com>
2024-09-23 08:50:17 -0700

    Time: 47 ms (84.22%), Space: 49.1 MB (44.13%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let right = nums.length - 1;
+    let left = 0;
+   
+    while(left <= right){
+        let mid = Math.floor((left+right) / 2);
+        if(nums[mid] === target){
+            return mid;
+        }
+        //  this checks if the left part is sorted
+        if( nums[mid] >= nums[left]){
+            if(target < nums[mid] && target >= nums[left]){
+                right = mid - 1;
+            }
+            else{
+                left = mid + 1;
+            }
+        }
+        // checks if the right is sorted
+        else if ( nums[mid] < nums[right] ){
+            if(target > nums[mid] && target <= nums[right]){
+                left = mid + 1;
+            }
+            else{
+                right = mid - 1;
+            }
+          
+        }
+    }
+    
+    return -1;
+};
