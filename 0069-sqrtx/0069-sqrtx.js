6d593346136221cbea24b8e1a57d1860a1a9db84
Ayomide Victor <midevictor90@gmail.com>
2024-09-23 10:22:59 -0700

    Time: 76 ms (55.13%), Space: 52.9 MB (30.90%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.js
+++ 0069-sqrtx/0069-sqrtx.js
@@ -1,19 +1,21 @@
-/**
- * @param {number} x
- * @return {number}
- */
 var mySqrt = function(x, left = 0, right = x, result = 0) {
+    // Base case: when left exceeds right, return the result
     if(left > right) {
-       return result
-       }
+        return result;
+    }
+
     let mid = ~~(left + (right - left) / 2);
-    if(mid * mid === x){
+
+    // Check if mid * mid equals x
+    if(mid * mid === x) {
         return mid;
     }
-    else if(mid * mid > x){
-        return mySqrt(x, left , mid - 1, result);
+    // If mid * mid is greater than x, search the left half
+    else if(mid * mid > x) {
+        return mySqrt(x, left, mid - 1, result);
     }
-    else{
+    // If mid * mid is less than x, update result and search the right half
+    else {
         return mySqrt(x, mid + 1, right, mid);
     }
 };
