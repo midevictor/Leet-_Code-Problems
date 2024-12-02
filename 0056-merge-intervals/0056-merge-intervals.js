59be2f0d94ce6a961011f6ae697bc5e3a9e3eb35
Ayomide Victor <midevictor90@gmail.com>
2024-12-02 01:39:39 +0100

    Time: 7 ms (81.81%), Space: 58.3 MB (68.99%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.js
+++ 0056-merge-intervals/0056-merge-intervals.js
@@ -3,23 +3,41 @@
  * @return {number[][]}
  */
 var merge = function(intervals) {
-    let length = intervals.length;
-    intervals.sort((a,b) => a[0] - b[0]);
+//     let length = intervals.length;
+//     intervals.sort((a,b) => a[0] - b[0]);
    
-    for(let i = 1; i < length; i++){
-        if(intervals[i][0] <= intervals[i-1][1]){
-            if(intervals[i][1] >= intervals[i-1][1]){
-                intervals.splice(i-1, 2, [intervals[i-1][0], intervals[i][1]]);
-            }
-            else{
-                intervals.splice(i, 1);
-            }
-            i--;
-            length--;
-            
+//     for(let i = 1; i < length; i++){
+//         //an overlap occurs
+//         if(intervals[i][0] <= intervals[i-1][1]){
+//             if(intervals[i][1] >= intervals[i-1][1]){
+//                 intervals.splice(i-1, 2, [intervals[i-1][0], intervals[i][1]]);
+//                 //starIndx, noofindxto remove, addIdx 
+//             }
+//             else{
+//                 intervals.splice(i, 1);
+//                 //remove just the currIdx
+//             }
+//             i--;
+//             //so as to avoid skipping the next indicies, cos i+1 ois now the new i
+//             length--;  
+//             //to avoid rhe loop from going beyond 
+//         } 
+//     }
+//     return intervals;
+    
+    intervals.sort((a, b) => a[0] - b[0]);
+    let result = [intervals[0]];
+    
+    for(let i = 1; i < intervals.length; i++){
+        let lastInt = result[result.length - 1];
+        if(intervals[i][0] <= lastInt[1]){
+            lastInt[1] = Math.max(intervals[i][1], lastInt[1]) 
         }
-        
+        else{
+            result.push(intervals[i])
+        }
     }
-    return intervals;
+    return result;
 			
 };
+//sort the intervals by the 1st integer in the array
