c6cbbedc2f3bebc74f0f6406251b0b995c385f7a
Ayomide Victor <midevictor90@gmail.com>
2024-12-02 14:58:49 +0100

    Time: 7 ms (75.37%), Space: 57.7 MB (78.03%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
@@ -3,19 +3,35 @@
  * @return {number}
  */
 var findDuplicate = function(nums) {
-    // Initialize an empty Set
-    const seen = new Set();
+    let slow = 0;
+    let fast = 0;
+    let init = 0;
     
-    // Iterate through each number in nums
-    for (let num of nums) {
-        // Check if the number is already in the Set
-        if (seen.has(num)) {
-            return num; // Duplicate found
-        }
-        // Add the number to the Set
-        seen.add(num);
+    while(true){
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+        if(slow === fast) break
     }
     
-    // In case there is no duplicate (not expected in this problem)
-    return -1;
+    while(true){
+        slow = nums[slow];
+        init = nums[init];
+        if(slow === init) break;
+    }
+    
+    return slow;
+
 };
+    // // Initialize an empty Set
+    // const seen = new Set();
+    // // Iterate through each number in nums
+    // for (let num of nums) {
+    //     // Check if the number is already in the Set
+    //     if (seen.has(num)) {
+    //         return num; // Duplicate found
+    //     }
+    //     // Add the number to the Set
+    //     seen.add(num);
+    // }
+    // // In case there is no duplicate (not expected in this problem)
+    // return -1;
