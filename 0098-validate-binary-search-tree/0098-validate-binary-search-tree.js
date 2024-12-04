ee7460bdbe7b261109624eef2ffbf6268f06ca78
Ayomide Victor <midevictor90@gmail.com>
2024-12-04 23:04:13 +0100

    Time: 0 ms (100.00%), Space: 52.7 MB (72.12%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
@@ -17,17 +17,22 @@ var isValidBST = function(root) {
     return dfs(root, -Infinity, Infinity);  
 };
 
-const dfs = function(node, minimum, maximum){
+const dfs= (node, minimum, maximum) => {
     if(node.val <= minimum || node.val >= maximum){
         return false;
     }
     if(node.left){
+        //node.left = val
+        //minimum = minimum
+        //maximum = node.val
         if(!dfs(node.left, minimum, node.val)){
             return false;
         }
-    }
-    
+    }  
     if(node.right){
+        //node.right = val
+        //node.val = minimum
+        //maximum = maximum
         if(!dfs(node.right, node.val, maximum)){
             return false;
         }
