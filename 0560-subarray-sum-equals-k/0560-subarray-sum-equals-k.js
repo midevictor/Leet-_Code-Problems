ee609825b6e36ab260c144a8463c307c7f4bdedc
Ayomide Victor <midevictor90@gmail.com>
2024-12-07 19:16:18 +0100

    Time: 14 ms (88.36%), Space: 60.3 MB (33.35%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
@@ -5,18 +5,33 @@
  */
 var subarraySum = function(nums, k) {
     
+//     let count = 0;
+    
+//     for(let i = 0; i < nums.length; i++){
+//         let sum = 0;
+//         for(let j = i; j < nums.length; j++){
+//             sum += nums[j];
+//             if(sum === k){
+//                 count++;
+//             }
+//         }
+//     }
+    
+//     return count;
+    
+    let prefixSum = 0;
     let count = 0;
+    let prefixCount = new Map();
+    prefixCount.set(0, 1)
     
-    for(let i = 0; i < nums.length; i++){
-        let sum = 0;
-        for(let j = i; j < nums.length; j++){
-            sum += nums[j];
-            if(sum === k){
-                count++;
-            }
+    for(let num of nums){
+        prefixSum += num;
+        if(prefixCount.has(prefixSum - k)){
+            count += prefixCount.get(prefixSum - k);
         }
+        
+        prefixCount.set(prefixSum, (prefixCount.get(prefixSum) || 0) +1);
+        
     }
-    
     return count;
-    
 };
