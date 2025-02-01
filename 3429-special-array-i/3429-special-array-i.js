83a84fc9f30dce6f7d5d5e56c1c2e9a95b312914
Ayomide Victor <midevictor90@gmail.com>
2025-02-01 16:26:02 +0100

    Time: 0 ms (100%), Space: 51.4 MB (77.21%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.js
+++ 3429-special-array-i/3429-special-array-i.js
@@ -15,9 +15,9 @@ var isArraySpecial = function(nums) {
             return true
         }
     }
-    if(nums.length <= 1){
-        return true;
-    }
+    // if(nums.length <= 1){
+    //     return true;
+    // }
 
     for(let i = 0; i < nums.length - 1; i++){
         if((isEven(nums[i]) && isEven(nums[i + 1])) || (isOdd(nums[i]) && isOdd(nums[i + 1]))){
