1037cdea813d915fbd9058f8cd468c9fb70597fa
Ayomide Victor <midevictor90@gmail.com>
2025-02-19 23:48:44 +0100

    Time: 13 ms (62.74%), Space: 59.8 MB (5.88%) - LeetHub

---
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {string}
+ */
+var getHappyString = function(n, k) {
+    let  result = [];
+    let chars = ["a", "b", "c"];
+
+    function backtrack (curr){
+        if(curr.length === n){
+            result.push(curr);
+            return
+        }
+
+        for(let ch of chars){
+            if(curr.length === 0 || curr[curr.length - 1] !== ch){
+                backtrack(curr + ch);
+
+            }
+        }
+
+    }
+
+    backtrack("");
+
+    return result.length >= k ? result[k-1] : "";
+    
+};
