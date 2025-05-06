29b5b64e4ad244015c220d29128b66e7f9cee71f
Ayomide Victor <midevictor90@gmail.com>
2025-05-06 18:07:36 +0100

    Time: 9 ms (62.98%), Space: 60 MB (37.02%) - LeetHub

--- 1160-letter-tile-possibilities/1160-letter-tile-possibilities.js
+++ 1160-letter-tile-possibilities/1160-letter-tile-possibilities.js
@@ -3,6 +3,8 @@
  * @return {number}
  */
 var numTilePossibilities = function(tiles) {
+    // this question is a bit related to fibonacci sequence but comes with a twist
+    // we have return a count of possible number 
     let count = 0;
     const freqMap = new Map();
 
@@ -13,14 +15,14 @@ var numTilePossibilities = function(tiles) {
 
     function backtrack() {
         // console.log(`Path: ${path}, FreqMap:`, Object.fromEntries(freqMap));
-
+        // loop through the frequency map
         for (let [ch, freq] of freqMap.entries()) {
             if (freq > 0) {
                 freqMap.set(ch, freq - 1);
                 count++;
 
                 backtrack();
-
+         // we undo because we could have another sequence that needs the ch we have gone thru
                 freqMap.set(ch, freq); // Undo choice
             }
         }
