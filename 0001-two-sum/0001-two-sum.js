15565d1048479ec20eca2b1ebdc6ba8f94a16f20
Ayomide Victor <midevictor90@gmail.com>
2025-06-16 02:44:25 +0100

    Time: 1 ms (87.19%), Space: 56.1 MB (20%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -3,29 +3,17 @@
  * @param {number} target
  * @return {number[]}
  */
-
-    // let result = [];
 var twoSum = function(nums, target) {
-//     for(let i = 0; i < nums.length; i++){
-//         for(let j = i + 1; j < nums.length; j++){
-//             if(nums[i] + nums[j] === target){
-//                 result.push(i);
-//                 result.push(j);
-//             }
-//         }
-         
-//     }
-//      return result;
     let map = new Map();
-    for (let i = 0; i < nums.length; i++){
+    for(let i = 0; i < nums.length; i++){
         let num1 = nums[i];
-        let num2 = target - num1;
-        if(map.has(num2))
-            {
-                return[i, map.get(num2)];
-            }
-        //if false, it sets the map with the num1 has key and indices as value
-        map.set(num1, i)
+        let num2 = target - num1
+        if(map.has(num2)){
+            return [i, map.get(num2)]
+        }
+        else{
+            map.set(num1 , i)
+        }
     }
-};  
     
+};
