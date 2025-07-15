ab2f7a17653429a7eaf7500a0fc69f357e89706b
Ayomide Victor <midevictor90@gmail.com>
2025-07-15 16:36:12 +0100

    Time: 1 ms (55.1%), Space: 56 MB (16.33%) - LeetHub

---
+++ 3396-valid-word/3396-valid-word.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} word
+ * @return {boolean}
+ */
+var isValid = function(word) {
+    if(word.length < 3){
+        return false;
+    }
+    let vowel = 0;
+    let consonant = 0;
+    for(ch of word){
+       
+        if(!/[a-zA-Z0-9]/.test(ch)){
+            return false
+        }
+        if(/[a-zA-Z]/.test(ch)){
+        let character = ch.toLowerCase();
+        if('aeiou'.includes(character)){
+            vowel++;
+        }else{
+            consonant++;
+        }
+        }
+    }
+    // if(vowel > 0 && consonant > 0){
+    //     return true;
+    // }
+    return vowel > 0 && consonant > 0;
+
+    
+};
