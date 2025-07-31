a5dd6c52509106686ee299d1dfe895725eb92cde
Ayomide Victor <midevictor90@gmail.com>
2025-07-31 23:12:52 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -11,41 +11,5 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(list1, list2) {
-   // create an intstance of a tempNode with head as zero and next as nill
-    let tempNode = new ListNode(0, null);
-    console.log(tempNode);
-    let currentNode = tempNode;
     
-    while(list1 && list2){
-        if(list1.val < list2.val ){
-            currentNode.next = list1;
-            list1 = list1.next;
-        }
-        else{
-            currentNode.next = list2;
-            list2 = list2.next;
-        }
-        currentNode = currentNode.next;
-    }
-    currentNode.next = list1 || list2;
-    
-    return tempNode.next;
-    
-//     let tempNode = new ListNode(0, null);
-// let currentNode = tempNode;
-
-// while (l1 && l2) {
-//     if (l1.val < l2.val) {
-//         currentNode.next = l1;
-//         l1 = l1.next;
-//     } else {
-//         currentNode.next = l2;
-//         l2 = l2.next;
-//     }
-//     currentNode = currentNode.next;
-// }
-
-// currentNode.next = l1 || l2;
-
-// return tempNode.next;
 };
