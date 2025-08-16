39b84553728ccfd1617942e6851b98e56afb0513
Ayomide Victor <midevictor90@gmail.com>
2025-08-16 08:23:27 -0700

    Time: 4 ms (91.43%), Space: 54.4 MB (89.63%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.js
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.js
@@ -3,21 +3,43 @@
  * @return {number}
  */
 var lengthOfLIS = function(nums) {
-    let dp = new Array(nums.length).fill(1);
+    // let dp = new Array(nums.length).fill(1);
 
-    for(let i = 1; i < nums.length; i++){
-        for(let j = 0; j < i; j++){
-            if(nums[j] >= nums[i]){
-                continue;
+    // for(let i = 1; i < nums.length; i++){
+    //     for(let j = 0; j < i; j++){
+    //         if(nums[j] >= nums[i]){
+    //             continue;
+    //         }
+    //         dp[i] = Math.max(dp[i], dp[j] + 1);
+
+    //     }
+    // }
+
+    // let LIS = 0;
+    // for( let c of dp){
+    //      LIS = Math.max(LIS, c)
+    // }
+    // return LIS;
+
+    let LIS = [];
+    for(let i = 0; i < nums.length; i++){
+        if(LIS.length === 0 || nums[i] > LIS[LIS.length - 1] ){
+            LIS.push(nums[i]);
+        } else{
+            let left = 0;
+            let right = LIS.length;
+            while(left < right){
+                let mid = Math.floor((left + right)/2);
+                if(LIS[mid] < nums[i]){
+                    left = mid + 1;
+                }else{
+                    right = mid;
+                }
+
             }
-            dp[i] = Math.max(dp[i], dp[j] + 1);
-
+            LIS[left] = nums[i];
         }
+        
     }
-
-    let LIS = 0;
-    for( let c of dp){
-         LIS = Math.max(LIS, c)
-    }
-    return LIS;
+    return LIS.length;
 };
