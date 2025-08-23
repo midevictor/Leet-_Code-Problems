0e89a094de724c54097c802c97bad7060ae10ab6
Ayomide Victor <midevictor90@gmail.com>
2025-08-23 12:33:35 -0700

    Time: 39 ms (41.12%), Space: 72 MB (68.44%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -3,57 +3,43 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    ///cretaes an empty array for the sum
-    let newArray = [];
-    //initilaize position of left
-    let left = 0;
-    //initialize the position  of right
-    let right = nums.length-1;
-    // if the length of nums is less than 3
     if(nums.length < 3){
-        return newArray;
+        return [];
     }
-    //sort nums
-    nums.sort((a,b)=>
-              {return a-b});
-    
-    for(let [index, num] of nums.entries()){
-        //checks if the first num > 0 cos the other nums will be gretaer than it and that wont lead to a sum of 0
-        if(num > 0){
-            return newArray;
-        }
-        //checks if the current num is not equal to the previous
-        if(num === nums[index-1]){
+    let output = []
+    nums.sort((a,b) => a-b);
+    for(let i = 0; i < nums.length; i++){
+        let left = i+1;
+        let right = nums.length-1;
+        if(nums[i] > 0){
+            return output;
+        } 
+        if(nums[i] === nums[i-1]){
             continue;
         }
-        left = index+1;
-        right = nums.length-1;
-        let temp = 0;
-        //while left position is less than right
+        let sum = 0;
         while(left < right){
-            temp = num + nums[left]+ nums[right];
-            
-            if(temp === 0){
-                newArray.push([num, nums[left], nums[right]])
+            sum = nums[i] + nums[left] + nums[right];
+            if(sum === 0){
+                output.push([nums[i] , nums[left] ,nums[right]]);
                 left++;
-                right--;
-                while(left < right && nums[left] == nums[left-1]){
-                    left++
-                }
-                while(left < right && nums[right] == nums[right+1]){
-                    right--
-                }
-            }
-            //sum too big
-            else if (temp > 0){
                 right--
+                while(left < right && nums[left] === nums[left-1]){
+                    left++;
+                }
+                while(left < right && nums[right] === nums[right+1]){
+                    right--;
+                }
+
             }
-            //sum too small
-            else if (temp < 0){
-                left++
+            else if(sum > 0){
+                right--;
+            }
+            else if(sum < 0){
+                left++;
             }
         }
     }
-    return newArray
+    return output;
     
 };
